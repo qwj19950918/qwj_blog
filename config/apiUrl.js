3cd52b45385b320ed4b303c47182fa2aa5ef9f83
QIWEIJIE <625132960@qq.com>
2020-04-27 19:28:59 +0800

    博客接口区分本地线上

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -1,4 +1,5 @@
-let ipUrl = 'http://127.0.0.1:7001/default/'
+let pathfrom = process.env.NODE_ENV === 'development' ? '127.0.0.1' : '45.76.172.8'
+let ipUrl = "http://"+pathfrom+":7001/default/"
 let servicePath = {
     getArticleList:ipUrl + 'getArticleList',    //首页文章列表接口
     getArticleById:ipUrl + 'getArticleById/',   //文章详细页内容接口
